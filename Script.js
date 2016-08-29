a23922e460cda17e677f340184d34b5f80355cb8
ProMike360 <mike.brouwer@edu-kw1c.nl>
2016-08-30 01:33:56 +0200

    Update Script.js
    Changed the timer value from 1sec to 3sec, and made it global.

--- Script.js
+++ Script.js
@@ -2,18 +2,20 @@
 //Created on 29-08-2016
 var extensionInf = {
 	version:'0.1 (Test-Release)',
-	creator:'ProMike360'
+	creator:'ProMike360',
+	timer: 3000
 };
 
 //Telling to the console the script is running
 console.log('Gamer.nl | Review redesign = Running');
 console.log('Version = ' + extensionInf.version);
 console.log('Created by ' + extensionInf.creator);
+console.log('Timer set to: ' + extensionInf.timer)
 
 //This code will run when the page is loaded
 $(document).ready(function(){
-    //After 1sec. the code will be activated (because when this code runs, the page is not fully loaded (this has something to do with the gamer.nl website))
-    setTimeout(runScript, 1000);
+    //After 3sec. the code will be activated (because when this code runs, the page is not fully loaded (this has something to do with the gamer.nl website))
+    setTimeout(runScript, extensionInf.timer);
 });
 
 function runScript() {
